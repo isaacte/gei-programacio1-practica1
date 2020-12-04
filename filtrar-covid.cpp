41e580cfed589857f22d420f9c5ea94b9e02d9c6
isaacte <isaac@ite2002.net>
2020-12-04 15:46:28 +0100

    Canvis a cometre: 	modificat:        filtrar-covid.cpp

--- filtrar-covid.cpp
+++ filtrar-covid.cpp
@@ -2,20 +2,22 @@
 #include <stdio.h>
 
 int main() {
+    // Inicialitzem les variables
     int contador = 0;
     char caracter;
 
-    scanf("%c", &caracter);
-    while( caracter != '#' ) {
-        if (caracter == ',')
+    scanf("%c", &caracter); //Escanegem el primer caràcter
+    while( caracter != '#' ) { //Mentre no sigui el caràcter d'escapament
+        if (caracter == ',') //Com que em de trobar 'Llei' després d'una coma
             scanf("%c", &caracter);
-                if (caracter == 'L'){
+                if (caracter == 'L'){ //Comprovem si el següent caràcter és una L
                     scanf("%c", &caracter);
-                        if (caracter == 'l'){
+                        if (caracter == 'l'){ //Comprovem si el següent caràcter és una l
                             scanf("%c", &caracter);
-                            if (caracter == 'e'){
+                            if (caracter == 'e'){ //Comprovem si el següent caràcter és una e
                                 scanf("%c", &caracter);
-                                if (caracter == 'i'){
+                                if (caracter == 'i'){ //Comprovem si el següent caràcter és una i
+                                //Mostrem tots els caràcters fins al final de la línea
                                     do{
                                         scanf("%c", &caracter);
                                     } while (caracter != ',');
@@ -27,6 +29,6 @@ int main() {
                             }
                         }
                 }
-        scanf("%c", &caracter);
+        scanf("%c", &caracter); //Seguim escanejant
    }
 }
